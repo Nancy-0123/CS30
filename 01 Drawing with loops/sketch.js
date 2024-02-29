83c94ed14d6669fbdab32e74a1158274eaa39ecd
Nancy-0123 <yhxhy123@gmail.com>
2024-02-29 13:05:00 -0600

    update
    1

--- 01 Drawing with loops/sketch.js
+++ 01 Drawing with loops/sketch.js
@@ -9,9 +9,14 @@
 let xPos, yPos;   //declaration only
 
 function setup() {
-  createCanvas(400, 300);
-  xPos =[width* 0.05, width* 0.95, width* 0.05, width* 0.95];
-  yPos = [height* 0.05, height* 0.05, height* 0.95, height* 0.95];
+  createCanvas(400, 400);
+  xPos = []; yPos = [];
+  initialWithLoops();
+  secondLoop();
+  thirdLoop();
+  forthLoop();
+  //xPos =[width* 0.05, width* 0.95, width* 0.05, width* 0.95];
+  //yPos = [height* 0.05, height* 0.05, height* 0.95, height* 0.95];
 }
 
 function draw() {
@@ -25,12 +30,42 @@ function mousePressed(){
   yPos.push(mouseY);
 }
 
+function initialWithLoops(){
+  //Lay down
+  for(let x = 10; x< width; x+=30){
+    xPos.push(x);
+    yPos.push(height);
+  }
+}
+
+function secondLoop(){
+  for(let x = 10; x< width; x+=30){
+    xPos.push(x);
+    yPos.push(0);
+  }
+}
+
+function thirdLoop(){
+  for(let y = 40; y< width; y+=30){
+    xPos.push(0);
+    yPos.push(y);
+  }
+}
+
+function forthLoop(){
+  for(let y = 20; y< width; y+=30){
+    xPos.push(width);
+    yPos.push(y);
+  }
+}
+
+
 function cornersAndMouseLoop(){
   let i = 0;
   while(i < xPos.length){
     let x = xPos[i];
     let y = yPos[i];
-    circle(x,y,mouseX,mouseY);
+    circle(x,y,20);
     line(x,y,mouseX, mouseY);
     i++;
   } 
--- 03 Drawing with Nested Loops/sketch.js
+++ 03 Drawing with Nested Loops/sketch.js
@@ -1,15 +1,41 @@
-// Project Title
-// Your Name
-// Date
-//
+// Gradient Background + Rested Loops
+// Nancy
+// Feb 29, 2024
+// Creating a gradient + drawing with nested loops
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let rectHeight = 10;
+let spacing = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
-  background(220);
+  gradientBackground();
+}
+
+function nestedLoops(){
+  //
+  //
+  for (let x = 0; x < 100; x += spacing){
+    for(let y = 0; y < 100; y += spacing){
+      circle(x,y,10);
+    }
+  }
+
+}
+
+function gradientBackground(){
+  // use a single loop to draw several rectangles
+  // then color them into a gradient
+  let y = 0; 
+  while(y< height){
+    let c = color(mouseX,map(0,10, 0, height, 0, 255), map(y,0,height,0,255));
+    fill(c);
+    rect(0,y,width,rectHeight);
+    y += rectHeight;
+  }
 }
