ce4fa3339368fdbc3a3bff2e7c0ddbd91e3b70c5
Yang7004548 <7004548@spsd.sk.ca>
2024-03-01 15:20:35 -0600

    3.1

--- 03 Drawing with Nested Loops/sketch.js
+++ 03 Drawing with Nested Loops/sketch.js
@@ -1,41 +1,74 @@
-// Gradient Background + Rested Loops
-// Nancy
-// Feb 29, 2024
+// Gradient Background + Nested Loops
+// Nancy 
+// Feb 29th, 2024
 // Creating a gradient + drawing with nested loops
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
 let rectHeight = 10;
-let spacing = 20;
+let circleSize = 20;
+let spacing = 25;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-}
-
-function draw() {
+  noStroke();
+  //noLoop();  //stop at the end of draw();
   gradientBackground();
+  nestedLoops();
+}   //ALT-SHIFT-F (format)
+
+function mousePressed() {
+  //draw();
+  gradientBackground();
+  nestedLoops();
 }
 
-function nestedLoops(){
-  //
-  //
-  for (let x = 0; x < 100; x += spacing){
-    for(let y = 0; y < 100; y += spacing){
-      circle(x,y,10);
+function draw() {   //OVER AND OVER, 60 fps
+
+}
+
+function circleDistance(x1, y1, x2, y2) {
+  //given two points (x1,y1) , (x2,y2), return the 
+  //straightline distance between.   //manhatten???
+  let a = abs(x1 - x2);
+  let b = abs(y1 - y2);
+  let c = sqrt(pow(a, 2) + pow(b, 2));
+  return c;
+  //dist();
+}
+
+function nestedLoops() {
+  // using a loop within a loop, generate a grid arrangement
+  // for some circles
+  for (let x = 0; x < width; x += spacing) { //COLUMN 0 20 40 60 80
+    for (let y = 0; y < height; y += spacing) { //ROWS 0 20 40 60 80
+      let d = round(circleDistance(x, y, mouseX, mouseY));
+      //close circles: differnt color
+      let currentSize = circleSize;
+      if (d > 100) {
+        fill(0);
+      }
+      else {
+        fill(255, 0, 0);
+        currentSize = circleSize * 1.2;
+      }
+      circle(x, y, currentSize);
+      fill(255);
+      textAlign(CENTER, CENTER);
+
+      text(d, x, y);
     }
   }
-
 }
 
-function gradientBackground(){
+function gradientBackground() {
   // use a single loop to draw several rectangles
   // then color them into a gradient
-  let y = 0; 
-  while(y< height){
-    let c = color(mouseX,map(0,10, 0, height, 0, 255), map(y,0,height,0,255));
+  let y = 0;
+  while (y < height) {
+    let c = color(mouseX, map(y, 0, height, 255, 0), map(y, 0, height, 0, 255));
     fill(c);
-    rect(0,y,width,rectHeight);
+    rect(0, y, width, rectHeight);
     y += rectHeight;
   }
+
 }
