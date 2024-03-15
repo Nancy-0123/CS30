8806a9bc9681c83c1e3e5f1ef55b382b66a0a531
Yang7004548 <7004548@spsd.sk.ca>
2024-03-15 15:20:36 -0600

    Friday

--- Perlin Noise Project (Terrain Generation)/sketch.js
+++ Perlin Noise Project (Terrain Generation)/sketch.js
@@ -5,12 +5,13 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let rectWidth = 10; 
-let heightTime = 0; 
+let rectWidth = 5; 
+let heightTime = 0;
+let averageHeight=0;  
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER);
+  rectMode(CORNERS);
   background(255);
   generateTerrain(); 
 }
@@ -20,18 +21,29 @@ function generateTerrain(){
   //rectangles of varying height
   let topHeight = 0;
   let topX = 0;
+  let noiseShift = 0.01;
+  let totalHeight = 0; 
+  let rectCount = 0;
+
   fill(0);
   for (let x = 0; x < width; x += rectWidth){
     rectHeight = noise(heightTime);
     rectHeight = map(rectHeight, 0, 1, 0, height);
-    rect(x, height, rectWidth, rectHeight);
-    heightTime += 0.01;
+    rect(x, height, rectWidth+x, height - rectHeight);
+    heightTime += noiseShift;
+    
 
-    if (topHeight < rectHeight*0.5){
-      topHeight = rectHeight*0.5;
+    if (topHeight < rectHeight){
+      topHeight = rectHeight;
       topX = x;
     }
+
+    totalHeight += rectHeight;
+    rectCount += 1; 
   }
+  averageHeight = totalHeight/ rectCount
+  drawAverage(); 
+
   drawFlag(topX, height-topHeight);
 }
 function draw() {
@@ -53,9 +65,13 @@ function keyPressed() {
   }
 }
 
-
 function drawFlag(x,y){
   fill(0)
   line(x, y, x, y-30);
   triangle(x, y-20, x+15, y-20, x, y-40);
+}
+
+function drawAverage(){
+  fill(255, 180, 0);
+  rect(0, height-averageHeight, width, height-averageHeight + 5);
 }
