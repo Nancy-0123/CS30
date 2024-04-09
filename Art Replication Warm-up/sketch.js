0edbe3927ad7231b77bb3578e5a027261511cb27
Yang7004548 <7004548@spsd.sk.ca>
2024-04-09 15:15:37 -0600

    Day 2

--- Art Replication Warm-up/sketch.js
+++ Art Replication Warm-up/sketch.js
@@ -5,21 +5,28 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let a,b;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  a=100;
+  b=100;
+  drawLines();
+  noLoop();
 }
 
-function draw() {
-  background(220);
-  drawLines();
-}
 
 function drawLines(){
   for(let i = 0; i <= 100; i++){
-    let x = random(200);
-    let y = random(200);
-    stroke(0);
-    line();
+    let x = random(200,width-200);
+    let y = random(100,height-100);
+    if (i%2===0){
+      line(a,b,x,b);
+      a=x;
+    }
+    else {
+      line(a,b,a,y);
+      b=y;
+    }
   }
 }
